caef2b86b7e4e71a95c6246c0a353732f5c016cc
nithees-sj <nitheessj@gmail.com>
2025-12-02 20:20:40 +0530

    Time: 31 ms (97.96%) | Memory: 206.8 MB (18.37%) - LeetSync

--- 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -1,35 +1,15 @@
 class Solution {
-    static final long MOD = 1_000_000_007;
-
+    private static final int MOD = 1000000007;
     public int countTrapezoids(int[][] points) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> freq = new HashMap<>();
+        for(int[] point : points) freq.merge(point[1], 1, Integer::sum);
 
-        for (int[] p : points) {
-            map.put(p[1], map.getOrDefault(p[1], 0) + 1);
+        long sum = 0, ans = 0;
+        for(int x : freq.values()) {
+            long val = x * (x - 1L) / 2L;
+            ans += val * sum;
+            sum += val;
         }
-
-        List<Long> A = new ArrayList<>();
-        for (int freq : map.values()) {
-            if (freq >= 2) {
-                long c = (long) freq * (freq - 1) / 2;
-                A.add(c % MOD);
-            }
-        }
-
-        if (A.size() < 2) return 0;
-
-        long S = 0, SS = 0;
-
-        for (long x : A) {
-            S = (S + x) % MOD;
-            SS = (SS + (x * x) % MOD) % MOD;
-        }
-
-        long ans = (S * S % MOD - SS + MOD) % MOD;
-
-        long inv2 = (MOD + 1) / 2;
-        ans = ans * inv2 % MOD;
-
-        return (int) ans;
+        return (int)(ans % MOD);
     }
 }
