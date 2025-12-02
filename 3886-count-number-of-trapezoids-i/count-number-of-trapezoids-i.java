9ecf76ec6740e4eb2c73ff1802abcc8494b3db16
nithees-sj <nitheessj@gmail.com>
2025-12-02 20:19:40 +0530

    Time: 39 ms (70.92%) | Memory: 207.7 MB (7.65%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -0,0 +1,38 @@
+class Solution {
+    static final long MOD = 1_000_000_007;
+
+    public int countTrapezoids(int[][] points) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        // Count points by y-coordinate
+        for (int[] p : points) {
+            map.put(p[1], map.getOrDefault(p[1], 0) + 1);
+        }
+
+        List<Long> A = new ArrayList<>();
+        for (int freq : map.values()) {
+            if (freq >= 2) {
+                long c = (long) freq * (freq - 1) / 2; // number of segments at that y
+                A.add(c % MOD);
+            }
+        }
+
+        if (A.size() < 2) return 0; // cannot form trapezoid
+
+        long S = 0, SS = 0;
+
+        for (long x : A) {
+            S = (S + x) % MOD;
+            SS = (SS + (x * x) % MOD) % MOD;
+        }
+
+        // Formula: (S^2 - SS) / 2
+        long ans = (S * S % MOD - SS + MOD) % MOD;
+
+        // multiply by inverse of 2 mod MOD
+        long inv2 = (MOD + 1) / 2; // since MOD is prime
+        ans = ans * inv2 % MOD;
+
+        return (int) ans;
+    }
+}
