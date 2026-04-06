46fbe65b059e2a71a8783390efa07f95f545eba5
nithees-sj <nitheessj@gmail.com>
2026-04-06 09:19:03 +0530

    Time: 3 ms (87.52%) | Memory: 43.1 MB (78.26%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,20 @@
-import java.util.Stack;
-
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            // Push opening brackets to stack
-            if (c == '(' || c == '{' || c == '[') {
+        for(char c:s.toCharArray()){
+            if(c=='('||c=='{'||c=='['){
                 stack.push(c);
-            } 
-            // For closing brackets
-            else {
-                if (stack.isEmpty()) return false; // nothing to match
-                char top = stack.pop();
+            }
+            else{
+                if (stack.isEmpty()) return false;
 
-                // Check for valid pair
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
+                int top = stack.pop();
+
+                if(c==')'&&top!='('||c=='}'&&top!='{'||c==']'&&top!='['){
                     return false;
                 }
             }
         }
-
-        // If stack is empty, all brackets matched
-        return stack.isEmpty();
+        return stack.empty();
     }
 }
