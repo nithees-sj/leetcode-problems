237b7bc66272639c37b150a5a3b213e79c1c57cd
nithees-sj <nitheessj@gmail.com>
2026-06-23 21:51:56 +0530

    Time: 5 ms (2.67%) | Memory: 44.1 MB (10.19%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -2,6 +2,6 @@ import java.util.*;
 class Solution {
     public void sortColors(int[] nums) {
         Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
+        System.out.print(Arrays.toString(nums));
     }
 }
