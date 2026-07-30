3cf619ca48c1dba7af922fda0c86cb763d807ab1
nithees-sj <nitheessj@gmail.com>
2026-07-30 20:52:49 +0530

    Time: 12 ms (7.31%) | Memory: 48.3 MB (6.81%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,22 +1,19 @@
+import java.util.*;
+
 class Solution {
     public void moveZeroes(int[] nums) {
-        if(nums.length == 0){
-            System.out.println(0);
-        }
-
         int i=0;
 
         for(int j=0;j<nums.length;j++){
-            if(nums[j] != 0){
-                nums[i]=nums[j];
+            if(nums[j]!=0){
+                nums[i] = nums[j];
                 i++;
             }
         }
-
         while(i<nums.length){
-            nums[i]=0;
+            nums[i] =0;
             i++;
         }
-        System.out.print(nums);
+        System.out.print(Arrays.toString(nums));
     }
 }
