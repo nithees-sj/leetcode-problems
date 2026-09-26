64cb1a41ee4aff9be355bfe0470a0697e0fd4600
nithees-sj <nitheessj@gmail.com>
2026-09-26 14:16:17 +0530

    Time: 1 ms (99.96%) | Memory: 42.8 MB (26.91%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -6,10 +6,10 @@ class Solution {
             int lastNumber = x%10;
             rev = (rev*10)+lastNumber;
             x = x/10;
+        }
             if(rev>Integer.MAX_VALUE|| rev<Integer.MIN_VALUE){
                 return 0;
             }
-        }
         return (int)rev;
     }
 }
